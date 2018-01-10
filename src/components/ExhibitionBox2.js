a953867f06aaf0fa77d176ff9d3b9ae3928bbc54
Malwine <malwine@mailbox.org>
2018-01-10 22:42:26 +0100

    use timeout

--- src/components/ExhibitionBox2.js
+++ src/components/ExhibitionBox2.js
@@ -5,33 +5,27 @@ import { Entity } from 'aframe-react';
 export default class ExhibitionBox2 extends React.Component {
 
   componentDidMount(){
-    const box = document.getElementById('benj')
-    //console.log(typeof box);
-    //console.log(new aframe.THREE.MeshBasicMaterial());
-    const boringMaterial1 = new aframe.THREE.MeshBasicMaterial({color: 0x880000})
-    const boringMaterial2 = new aframe.THREE.MeshBasicMaterial({color: 0x008800})
-    console.log(box);
-    const po = box.getAttribute('position')
-    console.log(po);
-    //console.log(box.getObject3D('mesh'));
-    // box.getObject3D('mesh').material = [
-    //   neatMaterial,
-    //   boringMaterial1,
-    //   boringMaterial2,
-    //   boringMaterial1,
-    //   boringMaterial1,
-    //   boringMaterial2
-    // ]
+    setTimeout( () => {
+      const box = document.getElementById('benj')
+      console.log(typeof box);
+      const boringMaterial1 = new aframe.THREE.MeshBasicMaterial({color: 0x880000})
+      const boringMaterial2 = new aframe.THREE.MeshBasicMaterial({color: 0x008800})
+      const neatMaterial = box.components.material.material // gets the original material with the texture
+      console.log(box);
+      box.getObject3D('mesh').material = [
+        neatMaterial,
+        boringMaterial1,
+        boringMaterial2,
+        boringMaterial1,
+        boringMaterial1,
+        boringMaterial2
+      ]
+    }, 100)
   }
 
   render() {
     return (
-      <Entity
-        id="benj"
-        material={{ src: "https://cdn.hyperdev.com/click-me.svg?1477239469954" }}
-        geometry={ {primitive: "box"} }
-        position={{x:0, y:0, z:-5}}
-      />
+      <a-entity id="benj" material="src:https://cdn.hyperdev.com/click-me.svg?1477239469954" geometry="primitive: box" position="0 0 -5" />
     )
   }
 }
